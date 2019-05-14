c8cd87c1b9b7ab686e0cc37d58205d364fb486d2
mehighh <47794103+mehighh@users.noreply.github.com>
2019-05-14 15:08:19 +0200

    indent & linebreak

--- obstacle.py
+++ obstacle.py
@@ -5,12 +5,16 @@ import random
 red = (255,0,0)
 
 class Obstacle:
-    def __init__(self,args):
-        self.screen = args.screen
-        self.size = [int(i / 3) for i in args.size]
+  def __init__(self,args):
+    self.screen = args.screen
+    self.size = [int(i / 3) for i in args.size]
 
-        self.width = 4
-        self.start_pos = [random.randint(0,self.size[0]),random.randint(0,self.size[1])]
-        self.end_pos = [random.randint(0,self.size[0]),random.randint(0,self.size[1])]
-    def draw(self):
-        pygame.draw.line(self.screen,red,self.start_pos,self.end_pos,self.width)
+    self.width = 4
+    self.start_pos = [random.randint(0,self.size[0]),
+        random.randint(0,self.size[1])]
+    self.end_pos = [random.randint(0,self.size[0]),
+        random.randint(0,self.size[1])]
+  def draw(self):
+    pygame.draw.line(self.screen,red,
+       
+        self.start_pos,self.end_pos,self.width)
